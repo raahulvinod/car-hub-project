70557e2a2cee0fedaf0ebc8b1bdfaaf223c57641
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-24 12:34:37 +0530

    SearchBar Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from 'next/image';
-import { Hero } from '@/components';
+import { CustomFilter, Hero, Searchbar } from '@/components';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-X padding-Y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalouge</h1>
+          <p>Explore the cars you might like</p>
+          <div className="home__filters">
+            <Searchbar />
+            <div className="home__filter-container">
+              <CustomFilter title="fuel" />
+              <CustomFilter title="year" />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import CustomButton from './CustomButton';
+import CustomFilter from './CustomFilter';
 import Footer from './Footer';
 import Hero from './Hero';
 import Navbar from './Navbar';
+import SearchManufacturer from './SearchManufacturer';
+import Searchbar from './Searchbar';
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  Searchbar,
+  CustomFilter,
+  SearchManufacturer,
+};
